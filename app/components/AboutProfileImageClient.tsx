7a7623c9819ab1a746c81a03fa2cb9e901b3a097
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2025-05-01 09:47:22 -0500

    text client wrapper

---
+++ app/components/AboutProfileImageClient.tsx
@@ -0,0 +1,6 @@
+// components/AboutProfileImage.client.tsx
+'use client';
+import AboutProfileImage from './AboutProfileImage';
+export default function AboutProfileImageClient() {
+  return <AboutProfileImage />;
+}
