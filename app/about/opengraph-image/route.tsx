30a7c49b6ce172a6d6330d53fc914a0113811226
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2025-05-01 14:10:38 -0500

    corrected image path

--- app/about/opengraph-image/route.tsx
+++ app/about/opengraph-image/route.tsx
@@ -8,7 +8,7 @@ import commaNumber from "comma-number";
 
 // Image
 const rauchgPhoto = toArrayBuffer(
-  readFileSync(join(process.cwd(), "public/images/krisyotam-color.jpg"))
+  readFileSync(join(process.cwd(), "public/images/krisyotam-colored.jpg"))
 );
 
 // Fonts
